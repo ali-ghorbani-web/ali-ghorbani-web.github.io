b84065107d126d1dda990266b244f726e5a175da
alighorba <161057755+alighorba@users.noreply.github.com>
2024-12-25 22:02:35 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function startCount(id,target) {
+    const step = document.getElementById(id);
+    step.textContent = "0";
+    let current = 0;
+
+    const increment = () => {
+      if (current < target) {
+        current++;
+        step.textContent = current;
+
+        setTimeout(increment, 2);
+      }
+    };
+    increment();
+  }
+
+  document.addEventListener("DOMContentLoaded", startCount("current1",228))
+  document.addEventListener("DOMContentLoaded", startCount("current2",934))
+  document.addEventListener("DOMContentLoaded", startCount("current3",554))
